378adcf84ebff70ac96ea02d8f5755b09c3fde30
Clara Zammar <clara.zammar@lau.edu>
2023-01-25 20:45:22 +0200

    Revert "My year was added"
    This reverts commit c9310c72ce3aa97a4331ab31aee71b13867cfb75.

--- Me.java
+++ Me.java
@@ -2,6 +2,5 @@ public class Me {
 	public static void main(String[] args){
 		System.out.println("Clara Zammar");
 		System.out.println("Computer Engineering");
-		System.out.println("1st Year");
 	}
 }
