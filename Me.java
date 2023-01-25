c9310c72ce3aa97a4331ab31aee71b13867cfb75
Clara Zammar <clara.zammar@lau.edu>
2023-01-25 20:42:05 +0200

    My year was added

--- Me.java
+++ Me.java
@@ -2,5 +2,6 @@ public class Me {
 	public static void main(String[] args){
 		System.out.println("Clara Zammar");
 		System.out.println("Computer Engineering");
+		System.out.println("1st Year");
 	}
 }
